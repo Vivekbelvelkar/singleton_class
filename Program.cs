0c72438957ff80595b3d6411fc1a6b77c860b8c0
vivek <vivek@VIVEK-03>
2024-12-01 17:16:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace singleton_class
+{
+    class myclass
+    {
+        public myclass()
+        {
+            Console.WriteLine("public constructor");
+        }
+        public static void mymethod()
+        {
+            Console.WriteLine("go to hell");
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            
+        }
+    }
+}
